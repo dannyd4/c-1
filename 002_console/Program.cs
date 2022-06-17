b218d4a6365d1b9e1c4c285fde076614e1c66f5a
Danny_D <tip505@mail.ru>
2022-06-17 11:40:29 +0300

    initial 002

---
+++ 002_console/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
+Console.Write("Enter your name");
+string username1 = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username1);
